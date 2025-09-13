09525369aa12265f2a9f9ccfb830c52f0e8a29cb
johnny8905 <joushva.like@gmail.com>
2025-09-13 15:20:01 +0530

    Version3

--- Calculator.py
+++ Calculator.py
@@ -1,3 +1,16 @@
 print("addition:",2+2)
 print("subtraction:", 5-3)
 print("multiplication", 3*10)
+print("multiplication", 5*20)
+# Simple Python Calculator
+
+
+# Perform calculations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+
+# Display results
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
